4521d079458d58e122b4dfc98efd7cb057ffa245
Arun Karoth <arun.karoth@binance.com>
2022-01-14 13:50:09 +0800

    fix solidity version

--- contracts/Helloworld.sol
+++ contracts/Helloworld.sol
@@ -1,6 +1,6 @@
 pragma solidity ^0.5.16;
 contract HelloWorld {
-    function sayHello() public pure returns(string){
+    function sayHello() public pure returns(string memory){
         return("hello world");
     }
 }
